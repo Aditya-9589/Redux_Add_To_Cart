e0ae1957720639cd14b01124b49421a541c23278
Aditya Hariharno <adityahariharno12@gmail.com>
2024-07-03 23:51:23 +0530

    .

--- Redux-Practice-Project-Ecommerce/src/ReduxStore/CartStore.jsx
+++ Redux-Practice-Project-Ecommerce/src/ReduxStore/CartStore.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import addReducer from './CartSlice';
+
 
 export const CartStore = configureStore({
     reducers: {
-
+        cart  : addReducer,
     }
 })
 
--- Redux-Practice-Project-Ecommerce/src/main.jsx
+++ Redux-Practice-Project-Ecommerce/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App.jsx';
+import './index.css';
+import { CartStore } from './ReduxStore/CartStore.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={CartStore} >
     <App />
+    </Provider>
   </React.StrictMode>,
 )
